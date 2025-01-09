2c534fa4af7c9928d869c425003e769ee6437aa4
christhoph <cristoph.solis@outlook.com>
2025-01-09 01:03:29 -0500

    fix: fix glitch when changing months that start on Saturday with 31 days

--- src/modules/calendar/Calendar.tsx
+++ src/modules/calendar/Calendar.tsx
@@ -14,7 +14,7 @@ export const Calendar = ({ children }: CalendarProps) => {
       <div className="h-full flex flex-col items-center justify-between bg-calendar-gradient gap-8 p-9 text-white">
         <CalendarHeader />
 
-        <div className="flex flex-col gap-1 text-center">
+        <div className="h-96 flex flex-col gap-1 text-center">
           <CalendarActions />
 
           <CalendarDayNames />
